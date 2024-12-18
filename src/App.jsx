cbe1a4f51a3d3d76dbb3d2fb5f42d4847dde913a
Marco Mechini <mechinimarco35@gmail.com>
2024-12-18 16:31:22 +0100

    refactoring: cambiato tipo allo stato newPost e modifica al salvatagio e al reset

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,19 @@ import { useState } from 'react'
 import data from './data/data'
 
 function App() {
-  const [post, setPost] = useState({
-    id: 0,
-    title: "",
-    content: ""
-  })
-
-  const handleInputData = (e) => {
-    setPost(post => ({
-      ...post,
-      [e.target.name]: e.target.value
-    }))
-  }
+  const [post, setPost] = useState(data)
+  const [newPost, setNewPost] = useState({ id: 0, title: '', content: '' })
+  //per aggiungere più campi inserire un nuovo state
 
   const sendPost = event => {
     event.preventDefault()
     //aggiungere il nuovo parametro ad un campo dentro l'oggetto sottostante
-    setPost([...post, { id: Date.now(), title: newPost }])
-
+    setPost([...post, { id: Date.now(), title: newPost.title, content: newPost.content }])
+    setNewPost({
+      id: 0,
+      title: '',
+      content: ''
+    })
   }
 
   const deletePost = (postId) => {
@@ -30,14 +25,16 @@ function App() {
     <>
       <div>
         <form onSubmit={sendPost}>
-
+          {/* creare un componente per l'input (don't repete your self) */}
           <label htmlFor="desc">Post content</label>
           <input
             type="text"
             name="title"
             id="desc"
-            value={post.title}
-            onChange={handleInputData}
+            value={newPost.title}
+            onChange={e => {
+              setNewPost(e.target.value)
+            }}
           />
           <button type='submit'>submit</button>
         </form>
